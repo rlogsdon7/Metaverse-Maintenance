b5d96c7b8b126f133c9ac84ee9fffddfcac7e92f
rlogsdon7 <112486258+rlogsdon7@users.noreply.github.com>
2022-12-07 09:42:07 -0500

    Update losant.py

--- src/Raspberry Pi/losant.py
+++ src/Raspberry Pi/losant.py
@@ -9,21 +9,20 @@ import random
 from tkinter import *
 from PIL import ImageTk, Image
 
-time.sleep(15)
 connection = obd.OBD()
 productUID = "com.ite.rlogsdon:metaverse_maintenance"
 port = I2C("/dev/i2c-1")
 card = notecard.OpenI2C(port, 0, 0)
 req = {"req":"hub.set"}
 req["product"] = productUID
-req["mode"] = "periodic"
+req["mode"] = "continuous"
 rsp = card.Transaction(req)
-req = {"req":"card.location.mode","mode":"continuous"}
-rsp = card.Transaction(req)
-req = {"req":"card.aux","mode":"track"}
-rsp = card.Transaction(req)
-req = {"req":"card.location.track","start":True,"file":"location.qo","heartbeat":True,"hours":24}
-rsp = card.Transaction(req)
+#req = {"req":"card.location.mode","mode":"continuous"}
+#rsp = card.Transaction(req)
+#req = {"req":"card.aux","mode":"track"}
+#rsp = card.Transaction(req)
+#req = {"req":"card.location.track","start":True,"file":"location.qo","heartbeat":True,"hours":24}
+#rsp = card.Transaction(req)
 req = {"req":"hub.sync"}
 rsp = card.Transaction(req)
 #print(rsp)
@@ -32,7 +31,7 @@ rpm = "/"
 speed = "/"
 timing = "/"
 throttle = "/"
-run = "/"
+run = 0
 mil = "/"
 purge = "/"
 barr = "/"
@@ -42,7 +41,18 @@ curr_count = 1
 
 def updateAttributes():
     global curr_count
-    print(curr_count)
+    global engLoad
+    global rpm
+    global speed
+    global timing
+    global throttle
+    global run
+    global mil
+    global purge
+    global barr
+    global volt
+    global absLoad
+    
     curr_count = curr_count + 1
     connection = obd.OBD()
     if connection.status() == OBDStatus.CAR_CONNECTED:
@@ -70,16 +80,16 @@ def updateAttributes():
         volt = cmd66.value.magnitude
         absLoad = cmd67.value.magnitude
     elif True:
-        engLoad = random.randrange(0,100)
-        rpm = random.randrange(0,7000)
-        speed = random.randrange(0,100)
+        engLoad = random.randrange(50,100)
+        rpm = random.randrange(1000,4000)
+        speed = random.randrange(15,65)
         timing = random.randrange(0,100)
-        throttle = random.randrange(0,100)
-        run = random.randrange(0,6000)
+        throttle = random.randrange(12,60)
+        run = run + 3
         mil = random.randrange(0,1)
         purge = random.randrange(0,100)
         barr = random.randrange(65,135)
-        volt = random.randrange(8,15)
+        volt = random.randrange(11,13)
         absLoad = random.randrange(0,100)
     if (curr_count % 5 == 0):
         req = {"req":"note.add"}
@@ -87,8 +97,7 @@ def updateAttributes():
         req["sync"] = True
         req["body"] = {"G":connection.status(),"E":engLoad,"RPM":rpm,"S":speed,"T":timing,"TP":throttle,"R":run,"M":mil,"EP":purge,"B":barr,"C":volt,"A":absLoad}
         rsp = card.Transaction(req)
-        print(rsp)
-    
+        rsp = card.Transaction(req)
     xspeed.config(text= "   " + str(speed) + "   ")
     xTP.config(text= "   " + str(throttle) + "   ")
     xRPM.config(text= "   " + str(rpm) + "   ")
@@ -98,68 +107,69 @@ def updateAttributes():
     xVoltage.after(3000,updateAttributes)
 
 root=Tk()
-root.geometry("1024x600")
+root.title("Metaverse Maintenance")
+root.geometry("1920x1080")
 img = ImageTk.PhotoImage(Image.open("Logo.png"))
 #Row 0
 logo = Label(root, image = img)
-logo.grid(row=0,column=0,columnspan=300,pady=30)
+logo.grid(row=0,column=0,columnspan=300,pady=70)
 #Row 1-3
-speedLabel=Label(root,text="Speed",font="times 24 bold")
+speedLabel=Label(root,text="Speed",font="times 48 bold")
 speedLabel.grid(row=1,column=1)
-speedHolder=Label(root,text ="          ",font=("times",60,"bold"),bg="white")
-speedHolder.grid(row=2,column=1,pady=10,padx=10,ipady=10)
-xspeed=Label(root,font=("times",60,"bold"),bg="white")
+speedHolder=Label(root,text ="          ",font=("times",140,"bold"),bg="white")
+speedHolder.grid(row=2,column=1,pady=10,padx=10,ipady=30)
+xspeed=Label(root,font=("times",140,"bold"),bg="white")
 xspeed.grid(row=2,column=1,pady=10,padx=50,ipady=10)
-speedUnit=Label(root,text="MPH",font="times 15 bold")
+speedUnit=Label(root,text="MPH",font="times 24 bold")
 speedUnit.grid(row=3,column=1)
 
-RPMLabel=Label(root,text="Revolutions Per Minute",font="times 24 bold")
+RPMLabel=Label(root,text="Revolutions Per Minute",font="times 48 bold")
 RPMLabel.grid(row=1,column=2)
-RPMHolder=Label(root,text ="          ",font=("times",60,"bold"),bg="white")
-RPMHolder.grid(row=2,column=2,pady=10,padx=50,ipady=10)
-xRPM=Label(root,font=("times",60,"bold"),bg="white")
+RPMHolder=Label(root,text ="              ",font=("times",140,"bold"),bg="white")
+RPMHolder.grid(row=2,column=2,pady=10,padx=50,ipady=30)
+xRPM=Label(root,font=("times",140,"bold"),bg="white")
 xRPM.grid(row=2,column=2,pady=10,padx=50,ipady=10)
-RPMUnit=Label(root,text="RPM",font="times 15 bold")
+RPMUnit=Label(root,text="RPM",font="times 24 bold")
 RPMUnit.grid(row=3,column=2)
 
-TPLabel=Label(root,text="Throttle Position",font="times 24 bold")
+TPLabel=Label(root,text="Throttle Position",font="times 48 bold")
 TPLabel.grid(row=1,column=3)
-TPHolder=Label(root,text ="          ",font=("times",60,"bold"),bg="white")
-TPHolder.grid(row=2,column=3,pady=10,padx=50,ipady=10)
-xTP=Label(root,font=("times",60,"bold"),bg="white")
+TPHolder=Label(root,text ="          ",font=("times",140,"bold"),bg="white")
+TPHolder.grid(row=2,column=3,pady=10,padx=50,ipady=30)
+xTP=Label(root,font=("times",140,"bold"),bg="white")
 xTP.grid(row=2,column=3,pady=10,padx=50,ipady=10)
-TPUnit=Label(root,text="%",font="times 15 bold")
+TPUnit=Label(root,text="%",font="times 24 bold")
 TPUnit.grid(row=3,column=3)
 
 #Row 4-6
 BlankLabel=Label(root,text="   ",)
 BlankLabel.grid(row=4,column=1,pady=20)
 #Row 5-7
-ENGLabel=Label(root,text="Engine Load",font="times 24 bold")
+ENGLabel=Label(root,text="Engine Load",font="times 48 bold")
 ENGLabel.grid(row=5,column=1)
-ENGHolder=Label(root,text ="          ",font=("times",60,"bold"),bg="white")
-ENGHolder.grid(row=6,column=1,pady=10,padx=10,ipady=10)
-xENG=Label(root,font=("times",60,"bold"),bg="white")
+ENGHolder=Label(root,text ="          ",font=("times",140,"bold"),bg="white")
+ENGHolder.grid(row=6,column=1,pady=10,padx=10,ipady=30)
+xENG=Label(root,font=("times",140,"bold"),bg="white")
 xENG.grid(row=6,column=1,pady=10,padx=50,ipady=10)
-ENGUnit=Label(root,text="%",font="times 15 bold")
+ENGUnit=Label(root,text="%",font="times 24 bold")
 ENGUnit.grid(row=7,column=1)
 
-VoltageLabel=Label(root,text="Controle Module Voltage",font="times 24 bold")
+VoltageLabel=Label(root,text="Controle Module Voltage",font="times 48 bold")
 VoltageLabel.grid(row=5,column=2)
-VoltageHolder=Label(root,text ="          ",font=("times",60,"bold"),bg="white")
-VoltageHolder.grid(row=6,column=2,pady=10,padx=50,ipady=10)
-xVoltage=Label(root,font=("times",60,"bold"),bg="white")
+VoltageHolder=Label(root,text ="          ",font=("times",140,"bold"),bg="white")
+VoltageHolder.grid(row=6,column=2,pady=10,padx=50,ipady=30)
+xVoltage=Label(root,font=("times",140,"bold"),bg="white")
 xVoltage.grid(row=6,column=2,pady=10,padx=50,ipady=10)
-VoltageUnit=Label(root,text="Volts",font="times 15 bold")
+VoltageUnit=Label(root,text="Volts",font="times 24 bold")
 VoltageUnit.grid(row=7,column=2)
 
-TimingLabel=Label(root,text="Timing Advance",font="times 24 bold")
+TimingLabel=Label(root,text="Timing Advance",font="times 48 bold")
 TimingLabel.grid(row=5,column=3)
-TimingHolder=Label(root,text ="          ",font=("times",60,"bold"),bg="white")
-TimingHolder.grid(row=6,column=3,pady=10,padx=50,ipady=10)
-xTiming=Label(root,font=("times",60,"bold"),bg="white")
+TimingHolder=Label(root,text ="          ",font=("times",140,"bold"),bg="white")
+TimingHolder.grid(row=6,column=3,pady=10,padx=50,ipady=30)
+xTiming=Label(root,font=("times",140,"bold"),bg="white")
 xTiming.grid(row=6,column=3,pady=10,padx=50,ipady=10)
-TimingUnit=Label(root,text="Degrees",font="times 15 bold")
+TimingUnit=Label(root,text="Degrees",font="times 24 bold")
 TimingUnit.grid(row=7,column=3)
 
 
